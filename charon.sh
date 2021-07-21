e6949c2d42be50cf7f8cf41ba1959835dbff0a41
felix <felix.ulmanu@gmail.com>
2021-07-21 10:52:04 -0400

    Working!

--- charon.sh
+++ charon.sh
@@ -1,6 +1,9 @@
 # Iterate over all words in input file
 pkgCount=$(grep -o -E '\w+' $1 | wc -l)
-for (( pkg=1; pkg<=$pkgCount; pkg++ ))
+for (( pkgNum=1; pkgNum<=$pkgCount-2; pkgNum++ ))
 do
-    awk -v pkg=$pkg 'FNR==pkg {print $0}' $1
+    #line=$(awk -v pkgNum=$pkgNum 'FNR==pkgNum {print $0}' $1)
+    # Replace all whitespace with one space and use cut to save individual package to variable
+    pkg=$(tr -s "\n" " " < $1 | cut -d " " -f $pkgNum)
+    paru -S $pkg --noconfirm
 done
